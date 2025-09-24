da38c8fa5dac478538075e9392518e698dfadecb
iruuuuu <irenemjh13@gmail.com>
2025-09-24 19:01:56 +0200

    Ejercise Name and Price with IVA made

--- Basicos_Javascript/src/ejercicio1.js
+++ Basicos_Javascript/src/ejercicio1.js
@@ -26,12 +26,42 @@ function suma (a=0 ,b=0){
 }
 
 
+
+//creo la funcion saludar
+function saludar (usuario){
+    //return "Hola ${usuario}";
+    return 'Bienvenido ${usuario ?? "Invitado"}';
+}
+
 ///---------------INICIALIZAR LA APLICACION------------------------
 
-//llamar a la funcion
-console.log(suma (3,5));
-console.log(suma (10,5));
-console.log(suma (7));
-console.log(suma ());
+// condicion ? sje realiza si es true ;se realiza si es false
+let edad=18;
+edad >=18 ? alert("Eres mayor de edad") :alert ("Eres menor de edad");
 
-suma,(4,6)
+// en caso de que esto sea null o undefined ?? hare esto otro
+let nombre = "Isaias"
+
+console.log(saludar(usuario));
+
+//llamar a la funcion
+
+console.log('La suma de 4+6 es : ${suma(4,6))   }');
+
+
+/*
+Funcion con un parametro llamada Aprobados como parametro un numero y me diga si estoy aprobado o no estoy aprobado
+crear una version 2.0 que si le paso un numero mayor a 9 me diga sobresaliente si es  entre 5 o 9 aprobado por debajo de  suspernso
+*/
+
+//Arrow function noob
+//const aprobados =  () => {
+//    return nota >=5 ? "Aprobado" : "Suspenso";
+//}  
+
+//Arrow function profesional
+const aprobados =  (nota =0) -->  nota>=5 ? "Aprobado" : "Suspenso";
+const aprobadosv2 =  (nota =0) --> (nota>=9 ? "Sobresaliente" : (nota>=5 ? "Aprobado" : "Suspenso"));
+
+
+console.log(aprobadosv2(9)); 
---
+++ Basicos_Javascript/src/ejercicio4.js
@@ -0,0 +1,76 @@
+//Ussode los arrays
+
+
+//Declaracion:
+
+const edades =[]; //array vacio
+
+const frutas = ["Manzana", "Pera", "Naranja", "Sandia"]; //array con elementos
+
+//usando constructor
+const cp = new Array();
+
+const cc = new Array("Madrid", "Barcelona", "Valencia");
+
+//como añadir elementos a un array --- ESTOS MUTAN EL ARRAY (original)
+edades.push(10);  //añade al final del array
+edades.unshift(10);  //añadir al comienzo
+
+//como eliminar elementos de un array
+edades.pop(); //elimina el ultimo elemento y retorna el elemento eliminado
+edades.shift(); //elimina el primer elemento y retorna el elemento eliminado
+
+
+//******** SLICE(extraer trozos de array y devuelve un nuevo array sin mutar el original)
+edades.slice(1,3); //extrae desde la posicion 1 hasta la 3 sin incluirla
+edades.slice(2); //extrae desde la posicion 2 hasta el final
+
+//ejemplo
+const edad = [1,2,3,4,5,6,7,8,9];
+numeros.slice(2,5); // [3,4,5]  ; esta seria la salida
+console.log(edad); // [1,2,3,4,5,6,7,8,9]  //el original no se ha mutado
+
+// ******** map 
+
+//una funcion que se pasa como parametro a otra funcion se lleama callback
+
+//si hay llaves hay return
+//si no hay llaves el return
+
+edades.map ( (edad)-->edad*2 ); // [2,4,6,8,10,12,14,16,18]   ; como salida es el array con los valores multiplicados por 2 sin haber mutado el original
+console.log(edad); // [1,2,3,4,5,6,7,8,9]  //el original no se ha mutado
+
+
+
+// ********** filter-filtra los elementos de un array y devuelve un nuevo array con los elementos que cumplen la condicion  quiere un  callback
+edades.filter( (edad)--> edad >=18); // [18]  ; como salida es el array con los valores mayores o iguales a 18 sin haber mutado el original
+
+
+
+
+//Dado un array de nombre crear una funcion llamada MAYUSCULAS que ponga en mayusculas todos los elementos de ese array
+
+const nombres = ["ana", "juan", "pepe", "maria"];
+function mayusculas (arrayNombres){
+    return arrayNombres.map ( (nombre) => nombre.toUpperCase() );
+}
+
+console.log(mayusculas(nombres)); // ['ANA', 'JUAN', 'PEPE', 'MARIA'
+
+//crear una funcion llamada PreciosconIVA que dado un array de precios me los devuelva con el IVA incluido
+const precios = [100, 200, 300, 400];
+function PreciosconIVA (arrayPrecios){
+    return arrayPrecios.map ( (precio) => (precio * 0.21) + precio );
+}
+console.log(PreciosconIVA(precios)); // [121, 242, 363, 484]
+
+//Crear una funcion llamada imparesCuadrados que le pase un array de numeros y me devuelva un array con los numeros impares elevados al cuadrado
+
+
+//crear una funcion llamada noramalizarEmail que le pase un array de emails que pueden llevar espacios al pincipio o al final del email y  me los devuelva sin espacios y en minusculasque
+
+
+//crear una funcion llamada Filtrarlongitud que le pase como parametro un array de nombres, un tamaño y me devuelva solo atraves de un array con los nombres cuya longitud es mayor o igual que el parametro que le he colocado
+
+
+//crear una funcion llamada normalizanombres propios que le pase un array de nombre y me los devuelva con la primera letra en mayusculas y el resto en minusculas
