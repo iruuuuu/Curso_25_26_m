d263b660d539c4af72d225d009e105e2044055b2
iruuu <irenemjh13@gmail.com>
2026-01-07 13:45:40 +0100

    CursoReact2526

---
+++ CursoReact2526/sesion-1/src/components/ejercicios/Recuerdo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+let nombreTS : string = 'TypeScript';
+let edadTS : number = 10;
+let isAdminTS: boolean = false;
+let nuloTS: null = null;
+let indefinidoTS: undefined = undefined;
+let arrayTS: number[] = [1, 2, 3, 4, 5];
+
+
+const Recuerdo = () => {
+  return (
+    <div>
+        Recuerdo
+    </div>
+  );
+}
+
+export default Recuerdo
